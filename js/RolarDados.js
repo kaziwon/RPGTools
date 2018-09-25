eb58f120f2fb1261ac6f478d3ef00dd0370c95e7
João Cabral <jv_cabral@outlook.com>
2018-09-25 16:01:04 -0300

    segundo commit

---
+++ js/RolarDados.js
@@ -0,0 +1,24 @@
+var dice = {
+    sides: 8,
+    roll: function () {
+      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
+    }
+  }
+  
+  
+  
+  
+  function printNumber(number) {
+    var placeholder = document.getElementById('placeholder');
+    placeholder.innerHTML = number;
+  }
+  
+  var button = document.getElementById('button');
+  
+  button.onclick = function() {
+    var result = dice.roll();
+    printNumber(result);
+  };
+  
+  
